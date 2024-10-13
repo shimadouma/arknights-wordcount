bf824ffb2be41cefb0ca22f71986a3ff3a818f6c
mimizuku <shimadouma@gmail.com>
2024-10-13 23:56:08 +0900

    updated script

--- list-events.sh
+++ list-events.sh
@@ -6,12 +6,28 @@ cd $(cd $(dirname $0); pwd)
 
 loc=ja_JP
 
-for dir in $(find ./ArknightsStoryJson/$loc/gamedata/story/activities/ -maxdepth 1 -mindepth 1 -type d); do
-    eventid=$(basename $dir)
-    json=$(find $dir -name "*.json" | head -n 1)
-    eventname=$(jq -r ".eventName" $json)
+tmp=`mktemp`
 
-    if [[ -n $eventname ]]; then
-        echo -e "$eventid\t$eventname"
-    fi
+trap 'rm -f $tmp' EXIT INT TERM HUP
+
+for loc in ja_JP zh_CN; do
+    echo "loc: $loc" >> $tmp
+    echo "" >> $tmp
+    for dir in $(find ./ArknightsStoryJson/$loc/gamedata/story/activities/ -maxdepth 1 -mindepth 1 -type d); do
+        eventid=$(basename $dir)
+        json=$(find $dir -name "*.json" | head -n 1)
+        eventname=$(jq -r ".eventName" $json)
+
+        if [[ -n $eventname ]]; then
+            echo -e "$eventid\t$eventname" >> $tmp
+        fi
+    done
+    echo "" >> $tmp
+    echo "" >> $tmp
 done
+
+if [[ -z $PAGER ]]; then
+    PAGER=less
+fi
+
+cat $tmp | $PAGER
